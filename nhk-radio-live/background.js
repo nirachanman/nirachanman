3ec61a775869073bb97cd311ef54f6435ed915c5
nirachanman <nirachanman@hotmail.com>
2026-01-29 21:06:46 +0900

    Delete nhk-radio-live directory

--- nhk-radio-live/background.js
+++
@@ -1,9 +0,0 @@
-chrome.action.onClicked.addListener((tab) => {
-  chrome.sidePanel.setOptions({
-    tabId: tab.id,
-    path: 'sidebar.html',
-    enabled: true
-  }, () => {
-    chrome.sidePanel.open({ tabId: tab.id });
-  });
-});
